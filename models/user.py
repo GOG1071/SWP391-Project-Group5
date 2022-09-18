f87b86b8095d45492038695fcfcba979c293f039
shine102 <nguyenngotunglam2@gmail.com>
2022-09-19 00:37:33 +0700

    add frontend register_seller.html, create HomeOwner Model, Admin Model

--- models/user.py
+++ models/user.py
@@ -11,5 +11,18 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
-    role = db.Column(db.String(120), nullable=False)
+    banned = db.Column(db.boolean, nullable=False, default=False)
 
+class HomeOwner(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    house_address = db.Column(db.String(120), nullable=False)
+    approved = db.Column(db.boolean, nullable=False, default=False)
+    banned = db.Column(db.boolean, nullable=False, default=False)
+
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
