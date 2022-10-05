e9e0a9b4ecdbecc8d89772ee60b089e4377aa474
shine102 <nguyenngotunglam2@gmail.com>
2022-10-05 13:57:49 +0700

    done register homeowner, fix register normal

--- flaskr/models/chat.py
+++ flaskr/models/chat.py
@@ -4,6 +4,7 @@ class Chat(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     messages = db.relationship('Message')
     sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     chat_id = db.Column(db.Integer, db.ForeignKey('chat.id'), nullable = False)
