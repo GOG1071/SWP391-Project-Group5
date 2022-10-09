4712c7df2440ce26f98236e1558f9e06e1a97bd4
gog1071 <goldendarkness1071@gmail.com>
2022-10-10 00:54:21 +0700

    add bookmarked homes controller

--- flaskr/routers/home.py
+++ flaskr/routers/home.py
@@ -11,3 +11,7 @@ def add_home():
     if request.method == "POST":
         return controllers.home.add_home()
     return render_template("add_home_for_owner.html")
+
+@home_router.route('/info/<int:id>',methods=["GET"])
+def info(id):
+    return controllers.home.info(id)
