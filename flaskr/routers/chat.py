1cb44abb15e6e6a9ee1e2cf0a22a475d57b8075b
Nhi Duong <linhnphe163269@fpt.edu.vn>
2022-11-01 16:21:32 +0700

    raw

--- flaskr/routers/chat.py
+++ flaskr/routers/chat.py
@@ -1,7 +1,8 @@
-import controllers.chat
 from decorators.authentication import login_required
-from flask import Blueprint, session
-
+from flask import Blueprint, request, render_template, session,redirect,url_for
+from decorators.authentication import login_required
+from models.chat import Chat
+import controllers.chat
 chat_router = Blueprint('chat_router', __name__)
 
 @chat_router.route("/chat_all")
@@ -13,7 +14,10 @@ def chat_all():
 @login_required
 def message_user(receiver_id):
     return controllers.chat.message_user(receiver_id)
-@chat_router.route("/send_message/<int:receiver_id>")
+@chat_router.route("/send_message",methods=["POST", "GET"])
 @login_required
-def send_message(sender_id,receiver_id):
-    return controllers.chat.send_message(sender_id,receiver_id)
+def send_message():
+    if request.method == "POST":
+        return controllers.chat.send_message(request.form['receiver_id'])
+    return redirect(url_for('chat_router.message_user', receiver_id = request.form['receiver_id']))
+
