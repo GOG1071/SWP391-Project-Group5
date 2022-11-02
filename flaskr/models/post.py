3b453dc07d59bb31d5d92d19611925e91789eafb
minhdlhe163242 <minhdlhe163242@fpt.edu.vn>
2022-11-02 10:19:16 +0700

    .

--- flaskr/models/post.py
+++ flaskr/models/post.py
@@ -1,6 +1,7 @@
 from models.model import db
 from datetime import datetime
 
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(1000), unique=False, nullable=False)
@@ -10,7 +11,8 @@ class Post(db.Model):
     image = db.relationship('PostImage', back_populates='post', uselist=False)
     upvotes = db.relationship('Upvote', back_populates="post")
     comments = db.relationship('Comment', backref='post', lazy=True)
-    reports = db.relationship('ReportPost', backref='post', lazy=True)   
+    reports = db.relationship('ReportPost', backref='post', lazy=True)
+
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -19,11 +21,13 @@ class Comment(db.Model):
     content = db.Column(db.String(1000), unique=False, nullable=False)
     timestamp = db.Column(db.DateTime, nullable=False)
 
+
 class Upvote(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     post = db.relationship("Post", back_populates="upvotes")
+
 
 class PostImage(db.Model):
     id = db.Column(db.Integer, primary_key=True)
